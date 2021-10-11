2891cf3297af4ed4939e96957e1d17384f3b936d
Purinut <reawjangpurinut@gmail.com>
2021-10-11 21:27:07 +0700

    using module.css [with bugs]

--- sukhaphala-drugstore/src/components/HeroSection/HeroSection.js
+++ sukhaphala-drugstore/src/components/HeroSection/HeroSection.js
@@ -1,21 +1,22 @@
 import image from "../../image/homebackground.jpg";
-import "./HeroSection.css";
+import classes from "./HeroSection.module.css";
 import Button from "../Button/Button";
 import { NavLink } from 'react-router-dom';
 
 function HeroSection() {
   return (
     <>
-      <img src={image} className="background-img" alt="background" />
-      <div className="hero-container">
+      <img src={image} className={classes["background-img"]} alt="background" />
+      <div className={classes["hero-container"]}>
         <h1> SUKhaphala </h1>
         <p> Online Drugstore, Pharmacy, Prescriptions & Health </p>
         {/* <button className="btn">Get your health back</button> */}
         
-       <NavLink  to='/Shop' >
+       <NavLink to='/Shop' >
         <Button
           Button_style="btn"
-          Button_text="Get your health back" />
+          Button_text="Get your health back" 
+        />
         </NavLink>
           
       </div>
