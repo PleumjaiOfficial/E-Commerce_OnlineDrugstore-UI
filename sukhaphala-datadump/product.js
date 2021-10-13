4f12c6695e358cf672f226b7d58ecce822291d6f
natthaphon-wr <natthaphon.win@gmail.com>
2021-10-13 14:23:04 +0700

    complete dump product

--- sukhaphala-datadump/product.js
+++ sukhaphala-datadump/product.js
@@ -3,14 +3,14 @@ const fs = require('fs');
 const url = 'mongodb+srv://winnr:4myteam@cluster0.95dro.mongodb.net/sukhapala?retryWrites=true&w=majority';
 
 const productSchema = new mongoose.Schema({
-    name: String,
-    image: {
-      data: Buffer, 
-      contentType: String},
-    description: String,
-    price: Number,
-    remain: Number,
-    health_goal: [String]
+  name: String,
+  image: {
+    data: Buffer, 
+    contentType: String},
+  description: String,
+  price: Number,
+  remain: Number,
+  health_goal: [String]
 });
 
 const redimgPath = './product-image/redpill.jpg';
@@ -20,12 +20,13 @@ const whiteimgPath = './product-image/whitepill.jpg';
 const purpleimgPath = './product-image/purplepill.jpg';
 const rainbowimgPath = './product-image/rainbowpill.jpg';
 
-const dumpdata = [
+const dumpData = [
   {
     name: "red pills",
     image: {
       data:fs.readFileSync(redimgPath),
-      contentType: 'image/jpeg'},
+      contentType: 'image/jpeg'
+    },
     description: `red is hot`,
     price: 320,
     remain: 100,
@@ -35,7 +36,8 @@ const dumpdata = [
     name: "blue pills",
     image: {
       data:fs.readFileSync(blueimgPath),
-      contentType: 'image/png'},
+      contentType: 'image/png'
+    },
     description: `blue is cool.`,
     price: 400,
     remain: 90,
@@ -45,7 +47,8 @@ const dumpdata = [
     name: "black pills",
     image: {
       data:fs.readFileSync(blackimgPath),
-      contentType: 'image/jpeg'},
+      contentType: 'image/jpeg'
+    },
     description: `black is heavy`,
     price: 500,
     remain: 10,
@@ -55,7 +58,8 @@ const dumpdata = [
     name: "white pills",
     image: {
       data:fs.readFileSync(whiteimgPath),
-      contentType: 'image/jpeg'},
+      contentType: 'image/jpeg'
+    },
     description: `white is bright`,
     price: 320,
     remain: 100,
@@ -65,7 +69,8 @@ const dumpdata = [
     name: "purple pills",
     image: {
       data:fs.readFileSync(purpleimgPath),
-      contentType: 'image/jpeg'},
+      contentType: 'image/jpeg'
+    },
     description: `purple is mystic `,
     price: 400,
     remain: 90,
@@ -75,7 +80,8 @@ const dumpdata = [
     name: "rainbow pills",
     image: {
       data:fs.readFileSync(rainbowimgPath),
-      contentType: 'image/jpeg'},
+      contentType: 'image/jpeg'
+    },
     description: `rainbow is pride`,
     price: 500,
     remain: 10,
@@ -84,8 +90,6 @@ const dumpdata = [
 ];
 
 main().catch(err => console.log(err));
-
-
 async function main() {
   //connect database
   try {
@@ -108,11 +112,9 @@ async function main() {
 
   //create multiple documents
   try{
-    Product.create(dumpdata);
+    Product.create(dumpData);
     console.log('create correctly');
   }catch(error){
     console.log('failed to create');
   }
-  
 }
-
