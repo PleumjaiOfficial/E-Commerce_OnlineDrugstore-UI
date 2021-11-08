4207cd45e4fc5d5b8de767fe7f0c915fd8daefe5
natthaphon-wr <natthaphon.win@gmail.com>
2021-11-08 11:29:42 +0700

    create footer component
    don't know how to display in App.js

--- sukhaphala-drugstore/src/App.js
+++ sukhaphala-drugstore/src/App.js
@@ -12,6 +12,7 @@ import CartList from './components/CartList/CartList';
 import AdminCreateProduct from './pages/Admin/AdminCreateProduct/AdminCreateProduct';
 import { AdminShop } from './pages/Admin/AdminShop/AdminShop';
 import AdminEditShop from './pages/Admin/AdminEditShop/AdminEditShop';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
@@ -35,6 +36,8 @@ const App = () => {
           <Route path='/AdminCreateProduct' component={AdminCreateProduct} />
           <Route path='/AdminShop' component={AdminShop} />
           <Route path='/AdminEditShop/:id' component={AdminEditShop} />
+          
+          <Route path='/Footer' component={Footer} />
         
         </Switch>
 
---
+++ sukhaphala-drugstore/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './Footer.module.css';
+
+const Footer = () => {
+  return (
+      <div className={classes["footer"]}>
+          <h1>This is Footer </h1>
+      </div>
+  )
+}
+
+export default Footer;
