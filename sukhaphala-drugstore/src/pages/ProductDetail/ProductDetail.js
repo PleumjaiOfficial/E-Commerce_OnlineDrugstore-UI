4f5e6587673cab78423bf027ac76d4805d490033
natthaphon-wr <natthaphon.win@gmail.com>
2021-11-11 23:01:30 +0700

    edit product detail page
    create layout
    remaining: show health goal, responsive

--- sukhaphala-drugstore/src/pages/ProductDetail/ProductDetail.js
+++ sukhaphala-drugstore/src/pages/ProductDetail/ProductDetail.js
@@ -1,86 +1,83 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../../components/Navbar/Navbar';
 import classes from './ProductDetail.module.css';
 import Axios from 'axios';
-import {useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useDispatch } from 'react-redux';
-import {add2Cart, add2CartAsync} from '../../redux/actions/cartActions';
+import { add2Cart, add2CartAsync } from '../../redux/actions/cartActions';
 import Footer from '../../components/Footer/Footer';
 
 const ProductDetail = () => {
 
-    const { id } = useParams();
-    const [data, setData] = useState([]);
-    const [numpack,setNumpack] = useState(1);
+	const { id } = useParams();
+	const [data, setData] = useState([]);
+	const [numpack, setNumpack] = useState(1);
 
-    const handleNumpack = (event) => {
-        setNumpack(event.target.value)
-    }
+	const handleNumpack = (event) => {
+		setNumpack(event.target.value)
+	}
 
-    //in displace have action
-    const dispatch = useDispatch();
+	//in displace have action
+	const dispatch = useDispatch();
 
-    useEffect(()=>{
-        Axios.get('http://localhost:5000/products/' + id)
-        .then(res=>{
-            console.log(res);
-            setData(res.data);
-        })
-        .catch(err =>{
-            console.log(err)
-        });
-    },[])
-    console.log(data);
+	useEffect(() => {
+		Axios.get('http://localhost:5000/products/' + id)
+			.then(res => {
+				console.log(res);
+				setData(res.data);
+			})
+			.catch(err => {
+				console.log(err)
+			});
+	}, [])
+	console.log(data);
 
-    // useEffect(() => {
-    //     dispatch(addadd()) 
-    // }, [dispatch])
+	// useEffect(() => {
+	//     dispatch(addadd()) 
+	// }, [dispatch])
 
+	return (
+		<>
+			<Navbar />
+			<div className={classes["productdetail-container"]}>
 
-    return (
-    <>
-    <Navbar />
-    
-        <div className={classes["productdetail-container"]}>
+				<div className={classes["productdetail-image"]}>
+					<img src={data.image} />
+				</div>
 
+				<div className={classes["productdetail-content"]}>
+					<div className={classes["content-info"]}>
+						<div className={classes["info-name"]}> {data.name} </div>
+						<p className={classes["info-desc"]}> {data.description} </p>
+					</div>
 
-            <div className={classes["productdetail-image"]}>
-                <img src={data.image} />
-            </div>
+					<div className={classes["content-buy"]}>
+						<div className={classes["buy-price"]}>
+							<p>Price :</p>
+							<div className={classes["price-value"]}>{data.price}</div>
+							<div className={classes["price-unit"]}>Bath</div>
+						</div>
 
-            <div className={classes["productdetail-content"]}>
+						<div className={classes["buy-qty"]}>
+							<p>Amount :</p>
+							<select onChange={handleNumpack} value={numpack}>
+									<option >1</option>
+									<option >2</option>
+									<option >3</option>
+									<option >4</option>
+							</select>
+							<div className={classes["buy-unit"]}>Pack</div>
+						</div>
 
-                <h2 className={classes["productdetail-topic"]}> {data.name} </h2>
-                
-                <p className={classes["productdetail-desc"]}> {data.description} </p>
+						<button className={classes["btn"]} onClick={() => 
+							dispatch(add2CartAsync({ ...data, amount: numpack }))}>Add to cart</button>						
+					</div>
+				</div>
+			</div>
 
-                <h3 className={classes["product-price"]}>
-                    <span>price</span>
-                    <span className={classes["product-price-value"]}>{data.price}</span>  
-                    <span className={classes["product-price-unit"]}>Bath</span> 
-                </h3>
-
-                <h3 className={classes["product-qty"]}> 
-                    <span>Amount</span>
-                    
-                    <select onChange={handleNumpack} value={numpack}>
-                        <option >1</option>
-                        <option >2</option>
-                        <option >3</option>
-                        <option >4</option>
-                    </select> 
-                    
-                    <span product-qty-unit> pack </span>
-                </h3>
-
-                {/* <button className={classes["btn"]}>Add to cart</button> */}
-                <button className={classes["btn"]} onClick = {() => dispatch(add2CartAsync({...data,amount: numpack}))}>Add to cart</button>
-
-            </div>
-        </div>
-    <Footer />
-    </>
-    )
+			<Footer />
+		</>
+	)
 }
 
 export default ProductDetail
