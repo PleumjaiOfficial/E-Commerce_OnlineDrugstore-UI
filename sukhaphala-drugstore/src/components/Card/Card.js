58a766eb1afe7c17e946f1dcb03811f9ec55a09d
natthaphon-wr <natthaphon.win@gmail.com>
2021-11-16 16:16:51 +0700

    edit product card and cartlist
    complete product card
    cartlist remaining: query product name and image from db by using product id

--- sukhaphala-drugstore/src/components/Card/Card.js
+++ sukhaphala-drugstore/src/components/Card/Card.js
@@ -4,40 +4,36 @@ import classes from './Card.module.css';
 
 const Card = (props) => {
 
-    
-    console.log(props.id)
-    return (
-        <div className={classes["card"]}>
+  console.log(props.id)
+  return (
+    <div className={classes["card"]}>
 
-                <div className={classes["card-image"]}>
-                        <img src={props.img} />
-                        {/* <img src="https://miro.medium.com/max/1400/1*XaU1wjTJK4ZYI8yI_SFuTg.png" /> */}
-                </div>
+      <div className={classes["card-image"]}>
+        <img src={props.img} />
+        {/* <img src="https://miro.medium.com/max/1400/1*XaU1wjTJK4ZYI8yI_SFuTg.png" /> */}
+      </div>
 
-                <h2 className={classes["card-price"]}>
-                    <span>{props.title}</span>
-                    <span>{props.price} Bath </span>
-                </h2>
+      <div className={classes["card-price"]}>
+        <p>{props.title}</p>
+        <p>{props.price} Bath </p>
+      </div>
 
+      <div className={classes["overlay"]}>
+        <div className={classes["card-content"]}>
+          <h2 className={classes["card-title"]}>
+            <span>{props.title}</span>
+          </h2>
 
-                <div className={classes["overlay"]}>
-                    <div className={classes["card-content"]}>
-                        <h2 className={classes["card-title"]}>
-                            <span>{props.title}</span>
-                        </h2>
-                            
-                        <p className={classes["card-desc"]}>
-                            {props.desc}
-                        </p>
+          <p className={classes["card-desc"]}>
+            {props.desc}
+          </p>
 
-                        <h3> Expore more... </h3>
-                        {/* <h3>on left eye icon can open</h3>
-                        <h3>on right chart icon can ปรื้นๆ</h3> */}
+          <h3> Expore more... </h3>
 
-                    </div>
-                </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default Card
--- sukhaphala-drugstore/src/components/CartListItem/CartListItem.js
+++ sukhaphala-drugstore/src/components/CartListItem/CartListItem.js
@@ -1,18 +1,18 @@
-import {useEffect,useState} from 'react'
+import { useEffect, useState } from 'react'
 import classes from './CartListItem.module.css'
-import { useSelector,useDispatch } from 'react-redux';
-import {getCart, updateAddCartAsync, updateSubCartAsync, deleteFromCartAsync, getCartAsync } from '../../redux/actions/cartActions';
+import { useSelector, useDispatch } from 'react-redux';
+import { getCart, updateAddCartAsync, updateSubCartAsync, deleteFromCartAsync, getCartAsync } from '../../redux/actions/cartActions';
 import axios from 'axios';
 import Button from 'react-bootstrap/Button';
 import ConfirmModal from '../ConfirmModal/ConfirmModal';
 
 export const CartListItem = (props) => {
 
-    //modal state of confirm modal
+  //modal state of confirm modal
   const [openConfirm, setOpenConfirm] = useState(false);
   //modal state of informative modal
 
-const handleOpenConfirm = () => {
+  const handleOpenConfirm = () => {
     setOpenConfirm(true);
   }
 
@@ -25,102 +25,111 @@ const handleOpenConfirm = () => {
     dispatch(deleteFromCartAsync(cart))
   }
 
-     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-     const dispatch = useDispatch();
-    useEffect( async () => {
-        let res = await axios.get("http://localhost:5000/carts/")
-        dispatch(getCart(res.data))
-    },[])
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+  const dispatch = useDispatch();
+  useEffect(async () => {
+    let res = await axios.get("http://localhost:5000/carts/")
+    dispatch(getCart(res.data))
+  }, [])
 
-    //Loading and click
-    function simulateNetworkRequest() {
-        return new Promise((resolve) => setTimeout(resolve, 1000));
+  //Loading and click
+  function simulateNetworkRequest() {
+    return new Promise((resolve) => setTimeout(resolve, 1000));
+  }
+
+  const [isLoadingSub, setLoadingSub] = useState(false);
+  const [isLoadingAdd, setLoadingAdd] = useState(false);
+  const [cartBufferSub, setCartBufferSub] = useState({});
+  const [cartBufferAdd, setCartBufferAdd] = useState({});
+  // const [cartBuffer, setCartBuffer] = useState({});
+  // const [operation,setOperation] = useState("")
+
+  //loading sub
+  useEffect(() => {
+    if (isLoadingSub === true) {
+      simulateNetworkRequest().then(() => {
+        setLoadingSub(false);
+        dispatch(updateSubCartAsync({ ...cartBufferSub, amount: cartBufferSub.amount - 1 }));
+        //dispatch(updateAddCartAsync({...cartBuffer,amount: cartBuffer.amount + 1}));
+      });
     }
+  }, [isLoadingSub]);
 
-    const [isLoadingSub, setLoadingSub] = useState(false);
-    const [isLoadingAdd, setLoadingAdd] = useState(false);
-    const [cartBufferSub, setCartBufferSub] = useState({});
-    const [cartBufferAdd, setCartBufferAdd] = useState({});
-    // const [cartBuffer, setCartBuffer] = useState({});
-    // const [operation,setOperation] = useState("")
+  const handleClickSub = (cartItem) => {
+    setLoadingSub(true);
+    setCartBufferSub(cartItem);
+  };
 
-    //loading sub
-    useEffect(() => {
-        if (isLoadingSub === true) {
-            simulateNetworkRequest().then(() => {
-                setLoadingSub(false);
-                dispatch(updateSubCartAsync({...cartBufferSub,amount: cartBufferSub.amount - 1}));
-                //dispatch(updateAddCartAsync({...cartBuffer,amount: cartBuffer.amount + 1}));
-            });
-        } 
-    }, [isLoadingSub]); 
+  //loading add
+  useEffect(() => {
+    if (isLoadingAdd === true) {
+      simulateNetworkRequest().then(() => {
+        setLoadingAdd(false);
+        dispatch(updateAddCartAsync({ ...cartBufferAdd, amount: cartBufferAdd.amount + 1 }));
+      });
+    }
+  }, [isLoadingAdd]);
 
-    const handleClickSub = (cartItem) => {
-        setLoadingSub(true);
-        setCartBufferSub(cartItem);
-    };
+  const handleClickAdd = (cartItem) => {
+    setLoadingAdd(true);
+    setCartBufferAdd(cartItem);
+  };
 
-    //loading add
-    useEffect(() => {
-        if (isLoadingAdd === true) {
-            simulateNetworkRequest().then(() => {
-                setLoadingAdd(false);
-                dispatch(updateAddCartAsync({...cartBufferAdd,amount: cartBufferAdd.amount + 1}));
-            });
-        } 
-    }, [isLoadingAdd]); 
+  return (
+    <div className={classes["cartlist"]}>
+      {/* Query Product Image from productId */}
+      <div className={classes["img-container"]}>
+        <span> <img src={props.cart.image} /></span>
+      </div>
 
-    const handleClickAdd = (cartItem) => {
-        setLoadingAdd(true);
-        setCartBufferAdd(cartItem);
-    };
+      <div className={classes["detail-container"]}>
+        {/* Query Product Name from productId */}
+        {/* <div className={classes["name"]}> {props.cart.productId}</div> */}
+        <div className={classes["product-name"]}> Product Name </div>
 
-    return (
-        <div className={classes["cartlist"]}>
-                <div className={classes["img-container"]}>    
-                    <span> <img src={props.cart.image} /></span>
-                </div>        
+        <div className={classes["action-container"]}>
+          <div className={classes["action-detail"]}>
+            <h3> Quantity </h3>
+            <div className={classes["quantity-edit"]}>
+              <Button 
+                onClick={() => { handleClickSub(props.cart) }}>
+                {isLoadingSub ? <i class="fa fa-spinner fa-spin"></i> : "-"}
+              </Button>
+              <div className={classes["amount-container"]}> 
+                <p>{props.cart.amount} </p> 
+              </div>
+              <Button 
+                onClick={() => { handleClickAdd(props.cart) }}>
+                {isLoadingAdd ? <i class="fa fa-spinner fa-spin"></i> : "+"}
+              </Button>
+            </div>
+          </div>
 
-                <div className={classes["detail-container"]}>             
-                    <span className={classes["name"]}> {props.cart.name} </span>
-                    <span> <h3> qty </h3></span>
-                    
-                    <div className={classes["amount-container"]}>
-                        <Button onClick = { () => {handleClickSub(props.cart)} }>
-                            {isLoadingSub ?  <i class="fa fa-spinner fa-spin"></i> : "-"} 
-                        </Button>
+          <div className={classes["action-detail-total"]}>
+            <h3> Total </h3>
+            <div className={classes["total"]}> 
+              <h2> {props.cart.price * props.cart.amount} Bath </h2> 
+            </div>
+          </div>
 
-
-                        <div className={classes["amount"]}> <h2>{props.cart.amount} </h2> </div>
-
-            
-                        <Button onClick = { () => {handleClickAdd(props.cart)} }>
-                            {isLoadingAdd ? <i class="fa fa-spinner fa-spin"></i> : "+"  } 
-                        </Button>
-                    </div>
-                </div>
-                    
-                <div className={classes["price-container"]}>
-                    <span> <h3> total </h3></span>
-                    <div className={classes["total"]}> <h2> {props.cart.price * props.cart.amount} Bath </h2> </div>
-                </div>
-
-                <div className={classes["remove-container"]}>
-                    <span> <h3> remove all </h3></span>
-                    <button onClick= {handleOpenConfirm} > X </button>
-                </div>
-
-                <ConfirmModal 
-                    open={openConfirm}
-                    onClose={handleCloseConfirm}
-                    title='Are you sure?'
-                    detail='Press confirm to remove this product'
-                    buttonConfirmText='Confirm'
-                    buttonCancelText='Cancel'
-                    buttonConfirm={() => handleRemove(props.cart._id)}
-                    buttonCancel={handleCloseConfirm}
-                />
-
+          <div className={classes["action-detail"]}>
+            <h3> remove all </h3>
+            <button onClick={handleOpenConfirm} > X </button>
+          </div>
         </div>
-    )
+      </div>
+
+      <ConfirmModal
+        open={openConfirm}
+        onClose={handleCloseConfirm}
+        title='Are you sure?'
+        detail='Press confirm to remove this product'
+        buttonConfirmText='Confirm'
+        buttonCancelText='Cancel'
+        buttonConfirm={() => handleRemove(props.cart._id)}
+        buttonCancel={handleCloseConfirm}
+      />
+
+    </div>
+  )
 }
