b952b43ee895a6c0b4369170ad666380d2f1a6b1
natthaphon-wr <natthaphon.win@gmail.com>
2021-11-19 15:28:01 +0700

    dummy product and add Freepik ref in footer

--- sukhaphala-datadump/product.js
+++ sukhaphala-datadump/product.js
@@ -5,70 +5,115 @@ dotenv.config();
 const url = `mongodb+srv://winnr:${process.env.DB_PASSWORD}@cluster0.95dro.mongodb.net/sukhapala?retryWrites=true&w=majority`
 
 const productSchema = new mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
   name: String,
-  image: String,
+  image: {
+    type: String,
+    default: 'http://localhost:5000/images/default-image.jpg'
+  },
   description: String,
   price: Number,
   remain: Number,
   healthGoal: [String]
 });
 
-const dumpData = [
+const dummyProduct = [
   {
-    _id: new mongoose.Types.ObjectId(),
-    name: 'red pills',
-    image: 'http://localhost:5000/images/redpill.jpg',
-    description: 'red is hot',
-    price: 320,
-    remain: 100,
-    healthGoal: ['brain', 'stress'],
+    name: 'Asparagus Distill',
+    image: 'http://localhost:5000/images/asparagus.png',
+    description: `Certain compounds in asparagus are metabolized to yield ammonia 
+                  and various sulfur-containing degradation products, 
+                  including various thiols and thioesters,which give urine a characteristic smell.`,
+    price: 569,
+    remain: 648,
+    healthGoal: ['Brain', 'Stress', 'Skin']
   },
   {
-    _id: new mongoose.Types.ObjectId(),
-    name: 'blue pills',
-    image: 'http://localhost:5000/images/bluepill.png',
-    description: 'blue is cool',
-    price: 400,
-    remain: 90,
-    healthGoal: ['face', 'sleep'],
+    name: 'Mixberry Distill',
+    image: 'http://localhost:5000/images/mixberry.png',
+    description: `Berries tend to have a good nutritional profile. 
+                  They’re typically high in fiber, vitamin C, 
+                  and antioxidant polyphenols.`,
+    price: 375,
+    remain: 115,
+    healthGoal: ['Digestion', 'Skin']
   },
   {
-    _id: new mongoose.Types.ObjectId(),
-    name: 'black pills',
-    image: 'http://localhost:5000/images/blackpill.jpg',
-    description: 'black is heavy',
-    price: 500,
-    remain: 10,
-    healthGoal: ['power', 'strength'],
+    name: 'Matrix Pills (Red Pill and Blue Pill)',
+    image: 'http://localhost:5000/images/matrix.png',
+    description: `The terms "red pill" and "blue pill" refer to a choice between 
+                  the willingness to learn a potentially unsettling or life-changing truth 
+                  by taking the red pill or remaining in contented ignorance with the blue pill.`,
+    price: 740,
+    remain: 232,
+    healthGoal: ['Immunity', 'Brain']
   },
   {
-    _id: new mongoose.Types.ObjectId(),
-    name: 'white pills',
-    image: 'http://localhost:5000/images/whitepill.jpg',
-    description: 'white is bright',
-    price: 320,
-    remain: 100,
-    healthGoal: ['brain', 'stress'],
+    name: 'Mix Nutrient',
+    image: 'http://localhost:5000/images/nutrient.png',
+    description: `Good nutrition is one of the keys to a healthy life. 
+                  You can improve your health by keeping a balanced diet. 
+                  You should eat foods that contain vitamins and minerals.`,
+    price: 60,
+    remain: 843,
+    healthGoal: ['Joints', 'Eyes', 'Fitness']
   },
   {
-    _id: new mongoose.Types.ObjectId(),
-    name: 'purple pills',
-    image: 'http://localhost:5000/images/purplepill.jpg',
-    description: 'purple is mystic',
-    price: 400,
-    remain: 90,
-    healthGoal: ['face', 'sleep'],
+    name: 'Super Pills',
+    image: 'http://localhost:5000/images/super.png',
+    description: `This pill is masterful pill. It can solve almost all problem.`,
+    price: 520,
+    remain: 760,
+    healthGoal: ['Joints', 'Heart', 'Fitness', 'Immunity', 'Skin']
   },
   {
-    _id: new mongoose.Types.ObjectId(),
-    name: 'rainbow pills',
-    image: 'http://localhost:5000/images/rainbowpill.jpg',
-    description: 'rainbow is pride',
-    price: 500,
-    remain: 10,
-    healthGoal: ['power', 'strength'],
+    name: 'Milky Way Pills',
+    image: 'http://localhost:5000/images/milky.png',
+    description: `Milk is an excellent source of vitamins and minerals, including “nutrients of concern,” 
+                  which are under-consumed by many populations. It provides potassium, B12, calcium and vitamin D, 
+                  which are lacking in many diets. Milk is also a good source of vitamin A, magnesium, zinc and thiamine.`,
+    price: 64,
+    remain: 587,
+    healthGoal: ['Bones']
   },
+  {
+    name: 'Compound Mineral',
+    image: 'http://localhost:5000/images/mineral.png',
+    description: `Minerals are important for your body to stay healthy. 
+                  Your body uses minerals for many different jobs, including keeping your bones, muscles, heart, 
+                  and brain working properly. Minerals are also important for making enzymes and hormones.`,
+    price: 250,
+    remain: 734,
+    healthGoal: ['Immunity', 'Prenatol', 'Brain']
+  },
+  {
+    name: 'Teachala Pills',
+    image: 'http://localhost:5000/images/tea.png',
+    description: `Tea, especially green tea, is a rich source of flavonoids, bioactive compounds that can lessen oxidative stress, 
+                  relieve inflammation, and provide other health benefits.`,
+    price: 550,
+    remain: 300,
+    healthGoal: ['Eyes', 'Hair']
+  },
+  {
+    name: 'Matrix Pills 2',
+    image: 'http://localhost:5000/images/matrix2.png',
+    description: `The terms "red pill" and "blue pill" refer to a choice between the willingness to learn a potentially unsettling or 
+                  life-changing truth by taking the red pill or remaining in contented ignorance with the blue pill. 
+                  This is new version "resurrect pill".`,
+    price: 444,
+    remain: 400,
+    healthGoal: ['Immunity', 'Brain', 'Sleep']
+  },
+  {
+    name: 'Vitamin C',
+    image: 'http://localhost:5000/images/vitaminc.png',
+    description: `Vitamin C (also known as ascorbic acid and ascorbate) is a vitamin found in various foods and sold as a dietary supplement. 
+                  It is used to prevent and treat scurvy. Vitamin C is an essential nutrient involved in the repair of tissue, 
+                  the formation of collagen, and the enzymatic production of certain neurotransmitters.`,
+    price: 45,
+    remain: 505,
+    healthGoal: ['Digestion']
+  }
 ];
 
 Main().catch(err => console.log(err));
@@ -84,17 +129,17 @@ async function Main() {
   //create model
   const Product = mongoose.model('Product', productSchema);
 
-  //delete all document
-  try{
-    await Product.deleteMany({});
-    console.log('delete correctly');
-  }catch(error){
-    console.log('fail to delete');
-  }
+  // //delete all document
+  // try{
+  //   await Product.deleteMany({});
+  //   console.log('delete correctly');
+  // }catch(error){
+  //   console.log('fail to delete');
+  // }
 
   //create multiple documents
   try{
-    Product.create(dumpData);
+    Product.create(dummyProduct);
     console.log('create correctly');
   }catch(error){
     console.log('failed to create');
--- sukhaphala-drugstore/src/components/Footer/Footer.js
+++ sukhaphala-drugstore/src/components/Footer/Footer.js
@@ -7,7 +7,8 @@ const Footer = () => {
       <div className={classes["footer"]}>
           <div className={classes["footer-left"]}>
             <div className={classes["foot-header"]}>SUKhaphala</div>
-            <div className={classes["details"]}>Term of services</div>
+            <p>Term of services</p>
+            <p>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></p>
             <div className={classes["copyright"]}>
               <AiOutlineCopyright />
               <det className={classes["details"]}>   2021 SUKhaphala. All rights reserved.</det>
@@ -15,8 +16,8 @@ const Footer = () => {
           </div>
           <div className={classes["footer-right"]}>
             <con className={classes["contact"]}>Contact Us</con>
-            <det className={classes["details"]}>sukha@phala.com</det>
-            <det className={classes["details"]}>+66 88 12345678 </det>
+            <div className={classes["details"]}>sukha@phala.com</div>
+            <div className={classes["details"]}>+66 88 12345678 </div>
             <div className={classes["social"]}>
               <div className={classes["facebook"]}>
                 <AiFillFacebook />
