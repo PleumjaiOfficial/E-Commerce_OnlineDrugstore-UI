20da7203fb549272d06161aefe8a9e0bf966b1aa
Pleumjai.official <pleumjai.official@gmail.com>
2021-11-21 13:40:07 +0700

    UI can show error
    but backend cann't work

--- sukhaphala-drugstore/src/pages/Register/Register.js
+++ sukhaphala-drugstore/src/pages/Register/Register.js
@@ -23,6 +23,9 @@ import axios from 'axios';
 import InfoModal from '../../components/InfoModal/InfoModal';
 import { set } from 'js-cookie';
 import { typography } from '@mui/system';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';  
+import IconButton from '@mui/material/IconButton';
 
 const theme = createTheme({
   palette: {
@@ -45,6 +48,7 @@ const Register = () =>  {
   const [lastnameError,setLastnameError] = useState(false)
   const [emailError,setEmailError] = useState(false)
   const [phoneError,setPhoneError] = useState(false)
+  const [phoneErrorFormat,setPhoneErrorFormat] = useState(false)
 
 
   const [address,setAddress] = useState({
@@ -53,10 +57,11 @@ const Register = () =>  {
     country: '',
     postcode: '',
   });
-  const [locationError,setLocationError] = useState(false)
-  const [districtError,setDistrictError] = useState(false)
-  const [countryError,setCountryError] = useState(false)
-  const [postcodeError,setPostcodeError] = useState(false)
+  // const [locationError,setLocationError] = useState(false)
+  // const [districtError,setDistrictError] = useState(false)
+  // const [countryError,setCountryError] = useState(false)
+  // const [postcodeError,setPostcodeError] = useState(false)
+
 
   const [credential,setCredential] = useState({
     password: '',
@@ -66,8 +71,6 @@ const Register = () =>  {
   const [rePasswordError,setRePasswordError] = useState(false)
 
   
-
-
   //state of modal: false -> close modal, true -> open modal
   const [ openInfo, setOpenInfo ] = useState(false);
   //infomation of the modal
@@ -92,60 +95,63 @@ const Register = () =>  {
 
   const handleSubmit = () => {
 
+    
 
     //check basic-info error
+     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
     if(basicInfo.firstname == ''){
       setFirstnameError(true)
-    }
+    } else {setFirstnameError(false)}
 
     if(basicInfo.lastname == ''){
       setLastnameError(true)
-    }
+    } else {setLastnameError(false)}
 
-    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if(basicInfo.email == ''){
+    if(basicInfo.email == '' || !(basicInfo.email.match(mailformat))){
       setEmailError(true)
-    }else if(basicInfo.email.match(mailformat)){
-      setEmailError(false)
     }else{
-      setEmailError(true)
+      setEmailError(false)
     }
 
     if(basicInfo.phone == ''){
       setPhoneError(true)
-    }else if(typeof(basicInfo.phone) !== 'number'){
-      setPhoneError(true)
+    }else {
+      setPhoneError(false)
     }
 
     //check address error
-    if(address.location == ''){
-      setLocationError(true)
-    }
+    // if(address.location == ''){
+    //   setLocationError(true)
+    // }else{setLocationError(false)}
 
-    if(address.district == ''){
-      setDistrictError(true)
-    }
+    // if(address.district == ''){
+    //   setDistrictError(true)
+    // }else{setDistrictError(false)}
 
-    if(address.country == ''){
-      setCountryError(true)
-    }
+    // if(address.country == ''){
+    //   setCountryError(true)
+    // } else {setCountryError(false)}
 
-    if(address.postcode == ''){
-      setPostcodeError(true)
-    }
+    // if(address.postcode == ''){
+    //   setPostcodeError(true)
+    // }else {setPostcodeError(false)}
+
+
 
     //check credential error
     if(credential.password == ''){
       setPasswordError(true)
-    }
+    } else {(setPasswordError(false))}
 
     if(credential.re_password == ''){
       setRePasswordError(true)
-    }
+    } else {setRePasswordError(false)}
 
     if(credential.password !== credential.re_password){
       setRePasswordError(true)
-    }
+    } else {setRePasswordError(false)}
+
 
     const createCustomer = () => {
       axios.post('http://localhost:5000/auth/register',
@@ -170,6 +176,7 @@ const Register = () =>  {
         })
       })
       .catch(error => {
+        console.log(error.response.data);
         handleOpenInfo(error.response.data);
       })
     }
@@ -237,6 +244,7 @@ const Register = () =>  {
                     value={basicInfo.firstname}
                     onChange={e => setBasicInfo({...basicInfo, firstname: e.target.value})}
                     error = {firstnameError}
+                    helperText= {firstnameError && "Invalid field"}
                   />
                 </Grid>
 
@@ -251,6 +259,7 @@ const Register = () =>  {
                     value={basicInfo.lastname}
                     onChange={e => setBasicInfo({...basicInfo, lastname: e.target.value})}
                     error = {lastnameError}
+                    helperText= {lastnameError && "Invalid field"}
                   />
                 </Grid>
 
@@ -265,6 +274,7 @@ const Register = () =>  {
                     value={basicInfo.email}
                     onChange={e => setBasicInfo({...basicInfo, email: e.target.value})}
                     error = {emailError}
+                    helperText={emailError && "user@mail"}
                   />
                 </Grid>
 
@@ -272,12 +282,14 @@ const Register = () =>  {
                   <TextField
                     required
                     fullWidth
+                    type="number"
                     id="PHONE NUMBER"
                     label="PHONE NUMBER"
                     name="PHONE NUMBER"
                     value={basicInfo.phone}
                     onChange={e => setBasicInfo({...basicInfo, phone: e.target.value})}
                     error = {phoneError}
+                    helperText= {phoneError && "Phone were number and 10-digit."}
                   />
                 </Grid>
               </Grid>
@@ -296,27 +308,27 @@ const Register = () =>  {
               <Grid container spacing={2}>
                 <Grid item xs={12}>
                   <TextField
-                    required
+                    // required
                     fullWidth
                     id="LOCATION"
                     label="LOCATION"
                     name="LOCATION"
                     value={address.location}
                     onChange={e => setAddress({...address, location: e.target.value})}
-                    error={locationError}
+                    // error={locationError}
                   />
                 </Grid>
 
                 <Grid item xs={12}>
                   <TextField
-                    required
+                    // required
                     fullWidth
                     id="DISTRICT"
                     label="DISTRICT"
                     name="DISTRICT"
                     value={address.district}
                     onChange={e => setAddress({...address, district: e.target.value})}
-                    error={districtError}
+                    // error={districtError}
                   />
                 </Grid>
 
@@ -324,14 +336,14 @@ const Register = () =>  {
                   <FormControl sx={{ width: 400 }}>
                     <InputLabel id="demo-multiple-checkbox-label">COUNTRY</InputLabel>
                     <Select 
-                      required
+                      // required
                       fullWidth
                       labelId="select-label"
                       id="select"
                       value={address.country}
                       label="COUNTRY"
                       onChange={e => setAddress({...address, country: e.target.value})}
-                      error={countryError}
+                      // error={countryError}
                     >
                       <MenuItem value='thailand'> Thailand </MenuItem>
                     </Select>
@@ -340,14 +352,14 @@ const Register = () =>  {
 
                 <Grid item xs={12}>
                   <TextField
-                    required
+                    // required
                     fullWidth
                     id="POSTCODE"
                     label="POSTCODE"
                     name="POSTCODE"
                     value={address.postcode}
                     onChange={e => setAddress({...address, postcode: e.target.value})}
-                    error={postcodeError}
+                    // error={postcodeError}
                   />
                 </Grid>
               </Grid>
@@ -375,6 +387,7 @@ const Register = () =>  {
                     value={credential.password}
                     onChange={e => setCredential({...credential, password: e.target.value})}
                     error={passwordError}
+                    helperText= {passwordError && "Invalid field"}
                   />
                 </Grid>
 
@@ -390,6 +403,7 @@ const Register = () =>  {
                     value={credential.re_password}
                     onChange={e => setCredential({...credential, re_password: e.target.value})}
                     error={rePasswordError}
+                    helperText= {rePasswordError && "Password not match"}
                   />
                 </Grid>
               </Grid>
